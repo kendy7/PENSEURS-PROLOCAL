414ee3fdacbbe3e58de6ab94b61e15fc71cd0e65
JEAN  BAPTISTE  Mackendy <37254548+kendy7@users.noreply.github.com>
2018-06-28 11:27:19 -0700

    Delete en_US.js

--- content/plugins/slideshow/admin/lang/en_US/en_US.js
+++
@@ -1,8 +0,0 @@
-/*
- Flexigrid rowS title for this section
-*/
-var flexi_TITLE_ROWS = {
-  'FLEX_MAIN_TITLE_NAME' : "Name",
-  'FLEX_MAIN_TITLE_STATUS' : "Active"
-};
-jsIn.addDict(flexi_TITLE_ROWS);
