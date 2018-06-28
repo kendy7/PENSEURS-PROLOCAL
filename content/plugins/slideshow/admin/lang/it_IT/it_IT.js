1a923a89ba0377ad582ff48d741388266e7a1b7f
JEAN  BAPTISTE  Mackendy <37254548+kendy7@users.noreply.github.com>
2018-06-28 11:27:56 -0700

    Delete it_IT.js

--- content/plugins/slideshow/admin/lang/it_IT/it_IT.js
+++
@@ -1,8 +0,0 @@
-/*
- Flexigrid rowS title for this section
-*/
-var flexi_TITLE_ROWS = {
-  'FLEX_MAIN_TITLE_NAME' : "Nome",
-  'FLEX_MAIN_TITLE_STATUS' : "Attiva"
-};
-jsIn.addDict(flexi_TITLE_ROWS);
