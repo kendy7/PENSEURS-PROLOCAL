fedb1786d922511db1f46049e20ebf261ff67ec3
nayou1 <Nayouk4@gmail.com>
2018-06-28 14:25:54 -0400

    Delete it_IT.js

--- bc-admin/platform/Admin_users/lang/it_IT/it_IT.js
+++
@@ -1,8 +0,0 @@
-/*
- Flexigrid rowS title for this section
-*/
-var flexi_TITLE_ROWS = {
-  'FLEX_MAIN_TITLE_NAME' : "Nome",
-  'FLEX_MAIN_TITLE_USERID' : "UserID"
-};
-jsIn.addDict(flexi_TITLE_ROWS);
