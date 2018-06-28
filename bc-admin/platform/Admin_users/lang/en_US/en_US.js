1d2ba8ac50b85ff7bd19796b6f9559cca9a1ac31
nayou1 <Nayouk4@gmail.com>
2018-06-28 14:25:39 -0400

    Delete en_US.js

--- bc-admin/platform/Admin_users/lang/en_US/en_US.js
+++
@@ -1,8 +0,0 @@
-/*
- Flexigrid rowS title for this section
-*/
-var flexi_TITLE_ROWS = {
-  'FLEX_MAIN_TITLE_NAME' : "Nom",
-  'FLEX_MAIN_TITLE_USERID' : "Identifiant"  
-};
-jsIn.addDict(flexi_TITLE_ROWS);
