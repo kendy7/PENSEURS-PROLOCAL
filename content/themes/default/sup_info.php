8f26cceb6cdc145c0bae4d261377d260a8f2ed20
JEAN  BAPTISTE  Mackendy <37254548+kendy7@users.noreply.github.com>
2018-06-28 14:00:10 -0700

    Add files via upload

---
+++ content/themes/default/sup_info.php
@@ -0,0 +1,15 @@
+﻿<?php
+// check request
+if(isset($_POST['id']) && isset($_POST['id']) != "")
+{
+
+
+    // get info id
+    $id_info = $_POST['id'];
+   $query = ("DELETE FROM ".$table_prefix."informations WHERE id = '".$id_info."' ");
+  // execute(" update ag_orders set archive=0 WHERE id = '".$orders."' ");
+    if (!$result = execute($query)) {
+        exit(mysql_error());
+    }
+}
+?>
