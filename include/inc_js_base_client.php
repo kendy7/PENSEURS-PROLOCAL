a70b8cf44e5a6b635eab59b030ae2b8ff76d1742
JEAN  BAPTISTE  Mackendy <37254548+kendy7@users.noreply.github.com>
2018-06-28 12:59:49 -0700

    Add files via upload

---
+++ include/inc_js_base_client.php
@@ -0,0 +1,9 @@
+<?php
+$is_included = get_included_files();
+if( $is_included[0] == (__FILE__) ) die('You have no permission for direct access to this file');
+?>
+<script src="<?php echo abs_client_path ?>/include/js/jquery.js"></script>
+<script src="<?php echo abs_client_path ?>/include/js/bootstrap.js"></script>
+<script src="<?php echo abs_client_path ?>/include/js/bootstrap-button.js"></script>
+<script src="<?php echo abs_client_path ?>/include/js/bootstrap-collapse.js"></script>
+<script src="<?php echo abs_client_path ?>/include/js/bootstrap_extensions.js"></script>
